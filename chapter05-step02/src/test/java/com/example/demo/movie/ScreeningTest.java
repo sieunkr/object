e434bf860bf9f5c7d0bfe7c213e9d8fec5948522
sieunkim <sieunkr@gmail.com>
2021-06-10 18:41:54 +0900

    chapter 05 - step02

---
+++ chapter05-step02/src/test/java/com/example/demo/movie/ScreeningTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.movie;
+
+import com.example.demo.money.Money;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScreeningTest {
+
+    @Test
+    void reserveTest() {
+
+        //given
+        
+
+        //when
+
+
+        //then
+
+
+    }
+}
