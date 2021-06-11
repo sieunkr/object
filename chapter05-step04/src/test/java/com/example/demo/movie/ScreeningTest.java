4dbb1d8df04c146fc6656a69e0d3aa4c6e743a23
sieunkim <sieunkr@gmail.com>
2021-06-11 15:08:50 +0900

    Update ScreeningTest.java

--- chapter05-step04/src/test/java/com/example/demo/movie/ScreeningTest.java
+++ chapter05-step04/src/test/java/com/example/demo/movie/ScreeningTest.java
@@ -16,12 +16,14 @@ class ScreeningTest {
 
         //given
         DiscountCondition discountCondition = new SequenceCondition(3);
-        AmountDiscountMovie movie = new AmountDiscountMovie(
+        Movie movie = new AmountDiscountMovie(
                 "반지의제왕",
                 Duration.ofHours(1),
                 Money.wons(12000),
                 Money.wons(1000),
                 discountCondition);
+
+        //Movie 추상클래스에 의존함 164페이지 확인
         Screening screening = new Screening(movie, 3,
                 LocalDateTime.of(2021, 06, 02, 14, 00));
         Money expectedFee = Money.wons(11000);
